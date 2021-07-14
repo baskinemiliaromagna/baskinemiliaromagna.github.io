3e5ec9165caa5c593c5c32197191711b0c692a74
UncleDan <uncledan74@gmail.com>
2021-07-14 12:42:51 +0200

    update

--- wp-login.php
+++ wp-login.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html lang="it-IT" prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb#">
+<html lang="it-IT">
 	<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	<title>Login &lsaquo; Baskin Emilia Romagna &#8212; WordPress</title>
-	<meta name="robots" content="max-image-preview:large, noindex, noarchive">
+	<meta name="robots" content="noindex, follow">
 <link rel="dns-prefetch" href="//s.w.org">
 <link rel="stylesheet" id="dashicons-css" href="https://www.baskinemiliaromagna.it/wp-includes/css/dashicons.min.css?ver=5.7.2" type="text/css" media="all">
 <link rel="stylesheet" id="buttons-css" href="https://www.baskinemiliaromagna.it/wp-includes/css/buttons.min.css?ver=5.7.2" type="text/css" media="all">
@@ -84,7 +84,7 @@ var pwsL10n = {"unknown":"Efficacia della password sconosciuta","short":"Molto d
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
-} )( "default", {"translation-revision-date":"2021-06-08 20:55:10+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"it"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s è deprecata sin dalla versione %2$s! Usa %3$s al suo posto. Prova a scrivere del codice più inclusivo."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
+} )( "default", {"translation-revision-date":"2021-06-09 09:08:38+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"it"},"%1$s is deprecated since version %2$s! Use %3$s instead. Please consider writing more inclusive code.":["%1$s è deprecata sin dalla versione %2$s! Usa %3$s al suo posto. Prova a scrivere del codice più inclusivo."]}},"comment":{"reference":"wp-admin\/js\/password-strength-meter.js"}} );
 </script>
 <script type="text/javascript" src="https://www.baskinemiliaromagna.it/wp-admin/js/password-strength-meter.min.js?ver=5.7.2" id="password-strength-meter-js"></script>
 <script type="text/javascript" src="https://www.baskinemiliaromagna.it/wp-includes/js/underscore.min.js?ver=1.8.3" id="underscore-js"></script>
@@ -96,7 +96,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="https://www.baskinemiliaromagna.it/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
-var userProfileL10n = {"user_id":"0","nonce":"e1a381383d"};
+var userProfileL10n = {"user_id":"0","nonce":"d9b45db045"};
 /* ]]> */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
@@ -104,7 +104,7 @@ var userProfileL10n = {"user_id":"0","nonce":"e1a381383d"};
 	var localeData = translations.locale_data[ domain ] || translations.locale_data.messages;
 	localeData[""].domain = domain;
 	wp.i18n.setLocaleData( localeData, domain );
-} )( "default", {"translation-revision-date":"2021-06-08 20:55:10+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"it"},"Your new password has not been saved.":["La tua nuova password non è stata salvata."],"Hide":["Nascondi"],"Show":["Visualizza"],"Confirm use of weak password":["Conferma l'uso della password debole."],"Hide password":["Nascondi password"],"Show password":["Mostra password"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
+} )( "default", {"translation-revision-date":"2021-06-09 09:08:38+0000","generator":"GlotPress\/3.0.0-alpha.2","domain":"messages","locale_data":{"messages":{"":{"domain":"messages","plural-forms":"nplurals=2; plural=n != 1;","lang":"it"},"Your new password has not been saved.":["La tua nuova password non è stata salvata."],"Hide":["Nascondi"],"Show":["Visualizza"],"Confirm use of weak password":["Conferma l'uso della password debole."],"Hide password":["Nascondi password"],"Show password":["Mostra password"]}},"comment":{"reference":"wp-admin\/js\/user-profile.js"}} );
 </script>
 <script type="text/javascript" src="https://www.baskinemiliaromagna.it/wp-admin/js/user-profile.min.js?ver=5.7.2" id="user-profile-js"></script>
 	<div class="clear"></div>
