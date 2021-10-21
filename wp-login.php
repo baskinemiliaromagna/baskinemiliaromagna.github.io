c627362f5641d60d80698543424fa3cd29ae3135
UncleDan <uncledan74@gmail.com>
2021-10-21 09:49:27 +0200

    informazioni per le società

--- wp-login.php
+++ wp-login.php
@@ -94,7 +94,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="https://www.baskinemiliaromagna.it/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
-var userProfileL10n = {"user_id":"0","nonce":"4d0cd638a7"};
+var userProfileL10n = {"user_id":"0","nonce":"389f6e1599"};
 /*  */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
