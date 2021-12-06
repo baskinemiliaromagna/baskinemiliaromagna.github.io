4bb1855519fb7e95816679410e5324901f42ac6e
UncleDan <uncledan74@gmail.com>
2021-12-06 12:31:50 +0100

    update interregionale

--- wp-login.php
+++ wp-login.php
@@ -94,7 +94,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="https://www.baskinemiliaromagna.it/wp-includes/js/wp-util.min.js?ver=5.8.2" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
-var userProfileL10n = {"user_id":"0","nonce":"3301503170"};
+var userProfileL10n = {"user_id":"0","nonce":"18d510af22"};
 /*  */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
