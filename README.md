0413a1e170547dd76a57c62b40cfd5c5a40b3ba4
Daniele Lolli <uncledan@uncledan.it>
2023-06-19 17:48:52 +0200

    update news 2023

--- README.md
+++
@@ -1,7 +0,0 @@
-# baskinemiliaromagna.github.io
-
-This repository is for the site [baskinemiliaromagna.github.io](https://baskinemiliaromagna.github.io).
-
- Formerly www.baskinemiliaromagna.it.
-
-The site is served by [GitHub Pages](https://pages.github.com/) from the branch [main](https://github.com/baskinemiliaromagna/baskinemiliaromagna.github.io/tree/main) on [GitHub](https://github.com/).
---
+++ wp-admin/edit.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Redirecting...</title>
+		<meta http-equiv="refresh" content="0;url=https://baskinemiliaromagna.github.io/wp-login.php?redirect_to=http%3A%2F%2Fbaskinemiliaromagna.test%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D4585&reauth=1">
+	</head>
+	<body>
+		<script type="text/javascript">
+			window.location = "https://baskinemiliaromagna.github.io/wp-login.php?redirect_to=http%3A%2F%2Fbaskinemiliaromagna.test%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D4585&reauth=1";
+		</script>
+
+		<p>You are being redirected to <a href="https://baskinemiliaromagna.github.io/wp-login.php?redirect_to=http%3A%2F%2Fbaskinemiliaromagna.test%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D4585&reauth=1">https://baskinemiliaromagna.github.io/wp-login.php?redirect_to=http%3A%2F%2Fbaskinemiliaromagna.test%2Fwp-admin%2Fedit.php%3Fsimply_static_page%3D4585&reauth=1</a></p>
+	</body>
+</html>
